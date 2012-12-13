ab419993cc7c9555d2e529837a019f69244164ad
yaron <yaron57@gmail.com>
2012-12-13 07:38:23 +0300

    New version: 0.7.1
    Change-Id: I5fed50cfdef73a7d98664e3df10229ac59fc028c

--- SemanticInternalObjects.php
+++ SemanticInternalObjects.php
@@ -9,7 +9,7 @@
 
 if ( !defined( 'MEDIAWIKI' ) ) die();
 
-define( 'SIO_VERSION', '0.7' );
+define( 'SIO_VERSION', '0.7.1' );
 
 $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'parserhook'][] = array(
 	'path' => __FILE__,
